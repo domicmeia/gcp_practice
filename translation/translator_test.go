db216bc2e3c8389ccf3d3db51fc749464428d412
domicmeia <dhdlqks55@gmail.com>
2024-06-23 18:55:20 +0900

    feat: REST handler

---
+++ translation/translator_test.go
@@ -0,0 +1,30 @@
+package translation_test
+
+import (
+	"testing"
+
+	"github.com/domicmeia/gcp_practice/translation"
+)
+
+func TestTranslate(t *testing.T) {
+	res := translation.Translate("hello", "english")
+	if res != "hello" {
+		t.Errorf(`expected "hello" but recieved "%s"`, res)
+	}
+
+	res = translation.Translate("hello", "german")
+	if res != "hallo" {
+		t.Errorf(`expected "hallo" but recieved "%s"`, res)
+	}
+
+	res = translation.Translate("hello", "finnish")
+	if res != "hei" {
+		t.Errorf(`expected "hei" but received "%s"`, res)
+	}
+
+	res = translation.Translate("hello", "dutch")
+	if res != "" {
+		t.Errorf(`expected "" but received "%s"`, res)
+	}
+
+}
